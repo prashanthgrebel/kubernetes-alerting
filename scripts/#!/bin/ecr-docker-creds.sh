9bce16356fe1acf1b0d4c6761ff552fbd0f8ca6d
prashanthgrebel <92351464+prashanthgrebel@users.noreply.github.com>
2025-10-08 23:05:42 +0530

    Create ecr-docker-creds.sh

---
+++ scripts/#!/bin/ecr-docker-creds.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NAMESPACE=$1
+kubectl create secret docker-registry ecr-creds \
+  --docker-server=798262717396.dkr.ecr.us-east-1.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$(aws ecr get-login-password --region us-east-1) \
+  --namespace="$NAMESPACE"
+
+# Patch the default service account to use the secret
+kubectl patch serviceaccount default \
+  -n "$NAMESPACE" \
+  -p '{"imagePullSecrets": [{"name": "ecr-creds"}]}'
+
+echo "ECR secret created and default service account patched in namespace '$NAMESPACE'."
